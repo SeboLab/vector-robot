36d5defbc32389c048bfd8e155e1cefc85a88da2
Spencer Ng <sng1488@gmail.com>
2020-10-29 18:02:52 -0500

    Format code

--- nodes/anki_vector.py
+++ nodes/anki_vector.py
@@ -4,9 +4,11 @@
 import rospy
 import anki_vector
 
-class VectorNode():
-	def __init__(self):
-		pass
 
-if __name__ == '__main__':
-	rospy.init_node("vector")
+class VectorNode:
+    def __init__(self):
+        pass
+
+
+if __name__ == "__main__":
+    rospy.init_node("vector")

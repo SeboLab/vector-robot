78378044819bc05c0d9e322c7dfb70b5514b2f93
Spencer Ng <sng1488@gmail.com>
2020-11-03 18:06:38 -0600

    Add camera module and more data publishing

--- nodes/anki_vector_core.py
+++ nodes/anki_vector_core.py
@@ -4,14 +4,21 @@
 This is a placeholder name/format; plan on dividing this up
 into different nodes/classes based on functionality
 """
-
+from threading import Thread
 import rospy
-from std_msgs.msg import String, Int16
+from rospy import Subscriber, Publisher
+from std_msgs.msg import String, Int16, Float32, Bool
 from geometry_msgs.msg import Vector3
 
 import anki_vector
 
 from behavior import Behavior
+from camera import Camera
+from anki_vector_ros.msg import Pose
+
+
+def create_camera_thread(robot):
+    Camera(robot)
 
 
 class VectorNode:
@@ -23,26 +30,71 @@ class VectorNode:
 
         self.behavior_control = Behavior(self.robot)
 
+        self.async_robot = anki_vector.AsyncRobot(enable_camera_feed=True)
+        self.async_robot.connect()
+
+        # Needs to be async due to continuous publishing
+        Thread(target=create_camera_thread, args=(self.async_robot)).start()
+
         # Animations
         # Could add list of animation names as topic, but not necessary for dev use
-        self.anim_sub = rospy.Subscriber("/anim/play", String, self.play_anim)
-        self.anim_trigger_sub = rospy.Subscriber(
+        self.anim_sub = Subscriber("/anim/play", String, self.play_anim)
+        self.anim_trigger_sub = Subscriber(
             "/anim/play_trigger", String, self.play_anim_trigger
         )
 
         # Audio
-        self.audio_sub = rospy.Subscriber("/audio/play", String, self.play_wav)
-        self.audio_vol_sub = rospy.Subscriber("/audio/vol", Int16, self.set_vol)
+        self.audio_sub = Subscriber("/audio/play", String, self.play_wav)
+        self.audio_vol_sub = Subscriber("/audio/vol", Int16, self.set_vol)
         self.audio_vol = 100
 
+        # Motors
+        self.head_motor_sub = Subscriber("/motors/head", Float32, self.set_head_motor)
+        self.lift_motor_sub = Subscriber("/motors/lift", Float32, self.set_lift_motor)
+        self.wheel_motors_sub = Subscriber(
+            "/motors/wheels", Float32, self.set_wheel_motors
+        )
+        self.stop_motors_sub = Subscriber("/motors/stop", Bool, self.stop_motors)
+
         # Info publishing
-        self.accel_pub = rospy.Publisher("/accel", Vector3, queue_size=1)
-        self.gyro_pub = rospy.Publisher("/gyro", Vector3, queue_size=1)
+        self.accel_pub = Publisher("/accel", Vector3, queue_size=1)
+        self.gyro_pub = Publisher("/gyro", Vector3, queue_size=1)
+        self.carry_object_pub = Publisher("/carry_object", Int16)
+        self.angle_pub = Publisher("/head_angle", Float32, queue_size=1)
+        self.tracking_pub = Publisher("/head_tracking_object", Int16, queue_size=1)
+        self.left_wheel_pub = Publisher("/wheel_speed/left", Float32, queue_size=1)
+        self.right_wheel_pub = Publisher("/wheel_speed/right", Float32, queue_size=1)
+        self.lift_height_pub = Publisher("/lift_height", Float32, queue_size=1)
+        self.localized_pub = Publisher("/localized_object", Int16)
+        self.pose_pub = Publisher("/pose", Pose)
+        self.pose_angle_pub = Publisher("/pose/angle", Float32)
+        self.pose_pitch_pub = Publisher("/pose/pitch", Float32)
+
+        # TODO handle NavMapComponents
+        # TODO continue with ProximityComponent in
+        # https://developer.anki.com/vector/docs/generated/anki_vector.robot.html#module-anki_vector.robot
+        # TODO create helper function to translation between anki_vector data object
+        # and a ROS message
 
         while not rospy.is_shutdown():
-            # Publish sensor data
+            # Publish sensor/motor data
             self.accel_pub.publish(self.robot.accel)
-            self.accel_pub.publish(self.robot.gyro)
+            self.gyro_pub.publish(self.robot.gyro)
+            self.angle_pub.publish(self.robot.head_angle_rad)
+            self.tracking_pub.publish(self.robot.head_tracking_object_id)
+            self.left_wheel_pub.publish(self.robot.left_wheel_speed_mmps)
+            self.right_wheel_pub.publish(self.robot.right_wheel_speed_mmps)
+            self.lift_height_pub.publish(self.robot.lift_height_mm)
+            self.localized_pub.publish(self.robot.localized_to_object_id)
+            self.carry_object_pub.publish(self.robot.carrying_object_id)
+
+            pose_msg = Pose()
+            for attr in dir(self.robot.pose):
+                setattr(pose_msg, attr, getattr(self.robot.pose, attr))
+
+            self.pose_pub.publish(pose_msg)
+            self.pose_angle_pub.publish(self.robot.pose_angle_rad)
+            self.pose_pitch_pub.publish(self.robot.pose_pitch_rad)
 
     def shutdown(self):
         print("Vector Robot shutting down...")
@@ -59,6 +111,19 @@ class VectorNode:
 
     def set_vol(self, vol):
         self.audio_vol = vol
+
+    def set_head_motor(self, speed):
+        self.robot.motors.set_head_motor(speed)
+
+    def set_lift_motor(self, speed):
+        self.robot.motors.set_lift_motor(speed)
+
+    def set_wheel_motors(self, speed):
+        self.robot.motors.set_wheel_motors(speed, speed)
+
+    def stop_motors(self, stop):
+        if stop:
+            self.robot.motors.stop_all_motors()
 
 
 if __name__ == "__main__":
---
+++ nodes/camera.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3.6
+
+# Credit to
+# https://github.com/betab0t/vector_ros_driver/blob/develop/src/vector_ros_driver/camera.py
+
+import rospy
+import cv_bridge
+import numpy
+
+from sensor_msgs.msg import Image
+
+
+class Camera:
+    def __init__(self, async_robot, publish_rate=10, image_frame_id="camera_link"):
+        self.async_robot = async_robot
+        async_robot.camera.init_camera_feed()
+        self.rate = rospy.Rate(publish_rate)
+        self.image_frame_id = image_frame_id
+        self.image_publisher = rospy.Publisher("/camera", Image, queue_size=1)
+        self.publish_camera_feed()
+
+    def publish_camera_feed(self):
+        bridge = cv_bridge.CvBridge()
+
+        while not rospy.is_shutdown():
+            image = bridge.cv2_to_imgmsg(
+                numpy.asarray(self.async_robot.camera.latest_image), encoding="rgb8"
+            )  # convert PIL.Image to ROS Image
+            image.header.stamp = rospy.Time.now()
+            image.header.frame_id = self.image_frame_id
+            self.image_publisher.publish(image)
+
+            # make sure to publish at required rate
+            self.rate.sleep()
